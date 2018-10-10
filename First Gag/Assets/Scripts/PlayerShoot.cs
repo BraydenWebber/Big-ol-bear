9784f016bf1d2033d8ff863d938e5ac7dfc622e6
Brayden <brayden.w491@stu.nebo.edu>
2018-10-10 10:29:06 -0600

    Added some functions like shoot projectile

---
+++ First Gag/Assets/Scripts/PlayerShoot.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerShoot : MonoBehaviour {
+
+    public Transform FirePoint;
+    public GameObject projectile;
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.RightControl))
+            Instantiate(projectile, FirePoint.position, FirePoint.rotation);
+		
+	}
+}
