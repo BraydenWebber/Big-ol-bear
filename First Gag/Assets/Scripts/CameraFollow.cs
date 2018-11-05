afa5889f508d1509edbb6b2a35fac63adab21033
Brayden <brayden.w491@stu.nebo.edu>
2018-11-05 10:10:22 -0700

    Fixed thing,Saved graphics assests

--- First Gag/Assets/Scripts/CameraFollow.cs
+++ First Gag/Assets/Scripts/CameraFollow.cs
@@ -2,34 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class CameraFollow : MonoBehaviour
 {
-
 
     public PlayerControl Player;
 
     public bool isFollowing;
 
-    //Camera position offset
 
+    // Camera position offset
     public float xOffset;
-
     public float yOffset;
 
-    //Use this for Initialization
-
+    // Use this for initialization
     void Start()
     {
         Player = FindObjectOfType<PlayerControl>();
+
         isFollowing = true;
     }
 
-    //Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         if (isFollowing)
         {
-            transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform, transform.position.z);
+            transform.position = new Vector3(Player.transform.position.x + xOffset, Player.transform.position.y + yOffset, transform.position.z);
 
         }
     }
