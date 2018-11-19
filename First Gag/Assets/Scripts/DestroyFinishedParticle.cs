d91d9d8a4471c673730fd80310b661deb290cc1c
Brayden <brayden.w491@stu.nebo.edu>
2018-11-19 10:09:21 -0700

    I mean it's better than it was last time

---
+++ First Gag/Assets/Scripts/DestroyFinishedParticle.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyFinishedParticle : MonoBehaviour {
+    private ParticleSystem thisParicleSystem;
+
+	// Use this for initialization
+	void Start () {
+        thisParicleSystem = GetComponent<ParticleSystem>();
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (thisParicleSystem.isPlaying)
+            return;
+        Destroy(gameObject);
+	}
+
+	void OnBecameInvisible()
+	{
+        Destroy(gameObject);
+	}
+}
